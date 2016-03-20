1f260e591c9a5e0d5ca1cf9f2c1c8450dff84f72
Engineering <akankshaagarwal007@gmail.com>
2016-03-20 14:36:01 +0530

    Practice Program

---
+++ ladder/lader.py
@@ -0,0 +1,27 @@
+#Akanksha Agarwal
+import re
+fin=open(lader_in.py)
+t=fin.readline()
+t=re.findall(r'\d+',t)
+t=list(t)
+test_case=int(t[0])
+flag=0
+a,b=2,5
+while(flag!=test_case):
+    vector=fin.readline()
+    vector=re.findall(r'\d+',vector)
+    vector=list(vector)
+    k=int(t[0])
+    curb=int(t[1])
+    # 2 or 5
+    
+    
+    
+        
+    
+    
+    
+    
+    
+    
+    
